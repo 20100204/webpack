7df7fcc3408121f0fc8d6440b9d08cc302349cd0
shixiao <274605391@qq.com>
2018-01-12 01:59:38 +0800

    two

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+const path = require('path');
 module.exports={
-    entry:'./src/app.js'
+    entry:'./src/app.js',
+    output:{
+        path:path.resolve(__dirname,'dist'),//这里要绝对路径
+        filename:'main.js',//输出的js文件名字
+    }
 }
