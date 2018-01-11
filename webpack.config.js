6d50ee0a5136e9dcbacaeb570aded84e63217ee1
shixiao <274605391@qq.com>
2018-01-12 02:27:40 +0800

    five

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports={
     entry:'./src/app.js',
     output:{
         path:path.resolve(__dirname,'dist'),//这里要绝对路径
         filename:'main.js',//输出的js文件名字
-    }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            filename:'abc.html',
+            template:'./src/index.html'
+        })
+    ]
 }
